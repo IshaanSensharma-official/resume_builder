d93644b8c8c8db447082b1dc57fccc181a67a500
Ishaan Sensharma <ishaans.official01@gmail.com>
2026-03-23 10:56:15 +0530

    Add functionality for sharing, visibility toggle, and downloading resumes in ResumeBuilder

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { ArrowLeft, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
+import { ArrowLeft, Briefcase, ChevronLeft, ChevronRight, DownloadIcon, EyeIcon, EyeOffIcon, FileText, FolderIcon, GraduationCap, Share2Icon, Sparkles, User } from 'lucide-react'
 import { dummyResumeData } from '../assets/assets'
 import PersonalInfoForm from '../components/PersonalInfoForm'
 import ResumePreview from '../components/ResumePreview'
@@ -54,6 +54,26 @@ const ResumeBuilder = () => {
     useEffect(()=>{
         loadExistingResume()
     },[resumeId])
+
+    const changeResumeVisibility = async () => {
+        setResumeData({...resumeData, public: !resumeData.public})
+    }
+
+    const handleShare = () =>{
+        const frontendUrl = window.location.href.split('/app')[0];
+        const resumeUrl = frontendUrl + '/view/' + resumeId;
+
+        if(navigator.share){
+            navigator.share({url: resumeUrl, text: "My Resume", })
+        }else{
+            alert('Share not supported on this browser')
+        }
+    }
+
+    const downloadResume = ()=>{
+        window.print();
+    }
+
 
     return (
         <div>
@@ -152,7 +172,28 @@ const ResumeBuilder = () => {
 
                     {/* Right Panel - Preview */}
                     <div className='lg:col-span-7 max-lg:mt-6'>
-                        <div>
+                        <div className='relative w-full'>
+                            <div className='absolute bottom-3 left-0 right-0 flex items-center
+                            justify-end gap-2'>
+                                {resumeData.public && (
+                                    <button onClick={handleShare} className='flex items-center p-2 px-4 gap-2 text-xs
+                                    bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600
+                                    rounded-lg ring-blue-300 hover:ring transition-colors'>
+                                        <Share2Icon className='size-4'/> Share 
+                                    </button>
+                                )}
+                                <button onClick={changeResumeVisibility} className='flex items-center p-2 px-4 gap-2 text-xs
+                                bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600
+                                ring-purple-300 rounded-lg hover:ring transition-colors'>
+                                    {resumeData.public ? <EyeIcon className="size-4"/> : <EyeOffIcon className="size-4"/>}
+                                    {resumeData.public ? 'Public' : 'Private'}
+                                </button>
+                                <button onClick={downloadResume} className='flex items-center gap-2 px-6 py-2 text-xs
+                                bg-gradient-to-br from-green-100 to-green-200 text-green-600
+                                rounded-lg ring-green-300 hover:ring transition-colors'>
+                                    <DownloadIcon className='size-4'/> Download 
+                                </button>
+                            </div>
                             {/* --- buttons ---- */}
                         </div>
 
