39ab5d7a0506953fb98d011e759ddfa7377d26b9
Ishaan Sensharma <ishaans.official01@gmail.com>
2026-03-27 22:12:24 +0530

    Fixed the errors of the resume routes under the Routes file

--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { createResume, deleteResume, getPublicResumeById, getResumeById, updateResume } from "../controllers/resumeController";
-import protect from "../middlewares/authMiddleware";
+import { createResume, deleteResume, getPublicResumeById, getResumeById, updateResume } from "../controllers/resumeController.js";
+import protect from "../middlewares/authMiddleware.js";
 import upload from "../configs/multer.js";
 
 const resumeRouter = express.Router();
 
 resumeRouter.post('/create', protect, createResume);
-resumeRouter.put('/update', XMLHttpRequestUpload.single('image'), protect, updateResume);
+resumeRouter.put('/update', upload.single('image'), protect, updateResume); 
 resumeRouter.delete('/delete/:resumeId', protect, deleteResume);
 resumeRouter.get('/get/:resumeId', protect, getResumeById);
 resumeRouter.get('/public/:resumeId', getPublicResumeById);
