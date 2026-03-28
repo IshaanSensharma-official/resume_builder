037043334cbd49c76a1c6a84732abc4ab24f629e
Ishaan Sensharma <ishaans.official01@gmail.com>
2026-03-28 17:02:34 +0530

    Added the login signup hidden feature when they are already completed by Dashboard icon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,8 @@ import Preview from "./pages/Preview"
 import Login from "./pages/Login"
 import { useDispatch } from "react-redux"
 import api from "./configs/api"
-import { login, setLoading } from "./app/features/authSlice"  // ✅ correct path
+import { login, setLoading } from "./app/features/authSlice"
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
 
@@ -40,6 +41,7 @@ const App = () => {
 
   return (
     <>
+    <Toaster />
       <Routes>
         <Route path='/' element={<Home />}/>
 
